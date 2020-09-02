a7beeaa7cffb608142fda01dfe1bc651f1cae454
rafolll <rafo.stva@gmail.com>
2020-09-01 21:06:30 -0500

    practica 3

--- Rafael Coaguila Benavente/empleosproyecto/src/main/java/ils/rafael/empleosproyecto/controller/Principal.java
+++ Rafael Coaguila Benavente/empleosproyecto/src/main/java/ils/rafael/empleosproyecto/controller/Principal.java
@@ -1,8 +1,14 @@
 package ils.rafael.empleosproyecto.controller;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ils.rafael.empleosproyecto.model.vacante;
 
 @Controller
 public class Principal {
@@ -14,5 +20,52 @@ public class Principal {
 		
 		return "home";
 	}
+	
+	
+	
+	@GetMapping("/detalle")
+	public String mostrarlistadetalle(Model model){
+        
+		vacante vacante1= new vacante();
+		vacante1.setId(1);
+		vacante1.setNombre("ingeniero de comunicaciones");
+		vacante1.setDescripcion("se necesita ingeniero para soporte intranet");
+		vacante1.setFecha(new Date());
+		vacante1.setSalario(8000.0);
+		
+		vacante vacante2= new vacante();
+		vacante2.setId(2);
+		vacante2.setNombre("ingeniero de Sistemas");
+		vacante2.setDescripcion("se necesita ingeniero de sistemas");
+		vacante2.setFecha(new Date());
+		vacante2.setSalario(10000.0);
+		
+		vacante vacante3= new vacante();
+		vacante3.setId(3);
+		vacante3.setNombre("Abogado");
+		vacante3.setDescripcion(" Se necesita abogado se paga poco");
+		vacante3.setFecha(new Date());
+		vacante3.setSalario(320.50);
+		
+		vacante vacante4= new vacante();
+		vacante4.setId(4);
+		vacante4.setNombre("Doctor");
+		vacante4.setDescripcion("se necesita Doctor con doctorado ");
+		vacante4.setFecha(new Date());
+		vacante4.setSalario(5650.0);
+		
+		
+		List<vacante>  lista = new LinkedList<>();
+		
+		lista.add(vacante1);
+		lista.add(vacante2);
+		lista.add(vacante3);
+		lista.add(vacante4);
+		
+		model.addAttribute( "vacantes" , lista );
+          	  	
+		return "detalle";
+	}
+	
 	
 }
---
+++ Rafael Coaguila Benavente/empleosproyecto/src/main/java/ils/rafael/empleosproyecto/model/vacante.java
@@ -0,0 +1,53 @@
+package ils.rafael.empleosproyecto.model;
+
+import java.util.Date;
+
+public class vacante {
+
+	private Integer   id;
+	private String  nombre;
+    private String   descripcion;
+    private  Date fecha;
+    private Double salario;
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Double getSalario() {
+		return salario;
+	}
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	@Override
+	public String toString() {
+		return "vacante [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fecha=" + fecha
+				+ ", salario=" + salario + "]";
+	}
+	
+    
+    
+    
+}

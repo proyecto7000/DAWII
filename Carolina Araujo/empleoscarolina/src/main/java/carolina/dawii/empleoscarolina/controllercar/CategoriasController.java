f20a5073392348d18c2a4f71419ad0d7a578fb40
carolina288 <70243026+carolina288@users.noreply.github.com>
2020-09-09 19:49:33 -0500

    p6 - Carolina Araujo

--- Carolina Araujo/empleoscarolina/src/main/java/carolina/dawii/empleoscarolina/controllercar/CategoriasController.java
+++ Carolina Araujo/empleoscarolina/src/main/java/carolina/dawii/empleoscarolina/controllercar/CategoriasController.java
@@ -2,28 +2,42 @@ package carolina.dawii.empleoscarolina.controllercar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value="/categorias")
 public class CategoriasController {
 
-	//@GetMapping("/index")
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String mostrarIndex(Model model) {
-		return "categorias/listaCategorias";
-	}
-	//@GetMapping("/create")
-	@RequestMapping(value="/create", method=RequestMethod.GET)
+
+	
+	
+	@GetMapping("/create")
+	//@RequestMapping(value="/create", method=RequestMethod.GET)
 	public String crear() {
 		return "categorias/formCategorias";
 	}
-	//@PostMapping("/save")
-	@RequestMapping(value="/save", method=RequestMethod.POST)
-	public String guardar() {
-		return "categorias/listaCategorias";
+	
+	
+	@PostMapping("/save")
+	//@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion) {
+		System.out.println("Nombre de categoria: " +nombre);
+		System.out.println("Descripcion de Catetgoria: " +descripcion);
+		return "/categorias/listaCategorias";
 	}
+	
+	
+		@GetMapping("/delete")
+		public String borrar(@RequestParam("id") int id, Model miModelo) {
+			//Procesamiento del parámetro. Aqui ya se hizo la conversion de String a int
+			System.out.println("RequestParam: "+ id);
+			miModelo.addAttribute("miId" , id);
+			return "categorias/mensaje";
+		}
 	
 	
 }
--- Carolina Araujo/empleoscarolina/src/main/java/carolina/dawii/empleoscarolina/controllercar/VacantesController.java
+++ Carolina Araujo/empleoscarolina/src/main/java/carolina/dawii/empleoscarolina/controllercar/VacantesController.java
@@ -1,10 +1,12 @@
 package carolina.dawii.empleoscarolina.controllercar;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/vacantes")
@@ -16,5 +18,17 @@ public class VacantesController {
 		model.addAttribute("idVacante", idVacante);
 		return "vacantes/detalle";
 	}
+	
+	@GetMapping("/delete")
+	public String borrar(@RequestParam("id") int id, Model miModelo) {
+		//Procesamiento del parámetro. Aqui ya se hizo la conversion de String a int
+		System.out.println("RequestParam: "+ id);
+		miModelo.addAttribute("miId" , id);
+		return "categorias/mensaje";
+	}
+	
+	
+
+	
 
 }

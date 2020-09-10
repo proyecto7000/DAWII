4ae2843828f4ce173c47a456f01959b76916912b
rafolll <rafo.stva@gmail.com>
2020-09-09 23:32:08 -0500

    Practica 6 - Rafael

--- Rafael Coaguila Benavente/empleosproyecto/src/main/java/ils/rafael/empleosproyecto/controller/categoriascontroller.java
+++ Rafael Coaguila Benavente/empleosproyecto/src/main/java/ils/rafael/empleosproyecto/controller/categoriascontroller.java
@@ -3,7 +3,9 @@ package ils.rafael.empleosproyecto.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value="/categorias")
@@ -18,18 +20,27 @@ public class categoriascontroller {
 	}
 	
 	@GetMapping("/create")
-	public String mostrarcrear(Model model){
-		
-		
+	public String mostrarcrear(){
 		return "categorias/formcategorias";
 	}
 	
 	
-	@GetMapping("/save")
-	public String guardado(Model model){
-		
-		
+	@PostMapping("/save")
+	public String guardado(@RequestParam("nombre") String elnombre, @RequestParam("descripcion") String ladescripcion){
+               System.out.println("nombre de la categoria: " + elnombre);
+               System.out.println("Descripcion : " + ladescripcion);
+		       
 		return "categorias/listacategorias";
 	}
+	
+	
+	@GetMapping("/delete")
+	public String borrar(@RequestParam("id") int id, Model model){
+       		
+		System.out.println("ELIMINADO CON ID: " + id);
+		  model.addAttribute("borrarid", id);
+		return "categorias/mensajeborrar";
+	}
+	
 	
 }

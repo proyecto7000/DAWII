43e44e68b4acee793ab6f8c834ab57ef936464ef
rafolll <rafo.stva@gmail.com>
2020-09-18 18:45:07 -0500

    Practica 11 - Rafael

--- Rafael Coaguila Benavente/empleosproyecto/src/main/java/ils/rafael/empleosproyecto/controller/vacantecontroller.java
+++ Rafael Coaguila Benavente/empleosproyecto/src/main/java/ils/rafael/empleosproyecto/controller/vacantecontroller.java
@@ -4,10 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ils.rafael.empleosproyecto.model.vacante;
 import ils.rafael.empleosproyecto.service.vacantesService;
@@ -55,20 +59,40 @@ public class vacantecontroller {
 	
 	
 	@GetMapping("/createvacante")
-	public String vacantenueva() 
+	public String vacantenueva( vacante vacante ) 
 	{
 		return "/vacantes/formvacante";
 	}
 	
-	@PostMapping("/savevacante")
 	
-	public String guardarvacante (vacante vacante )
+	
+	
+	
+	@PostMapping("/savevacante")
+	public String guardarvaca(vacante vacante, BindingResult resultado,RedirectAttributes Attributes) 
 	{
 		
-		System.out.println (vacante.toString());
+		for(ObjectError error: resultado.getAllErrors()){
+
+                         System.out.println("Ocurrio un error en : " +  error.getDefaultMessage()); 
+		}
 		
 		
-		 return "/vacantes/listavacantes";
+		if(resultado.hasErrors()){
+			return "/vacantes/formvacante";
+		}
+		
+		String   msje="Vacante agregada exitosamente";
+		
+            Attributes.addFlashAttribute("mensaje", msje);
+		
+		
+		
+		
+		
+		System.out.println(vacante.toString());
+		vacanteservicio.guardarvacante(vacante);
+		 return "redirect:/";
 	}
 	
 	@InitBinder

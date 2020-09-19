4e5005f931262e5af0ec003dceefd7f5b421c76c
walter2703 <59626631+walter2703@users.noreply.github.com>
2020-09-18 22:56:46 -0500

    Practica 11

---
+++ Walter Laura/EmpleosWalter/src/main/java/Practica/EmpleosWalter/Modelo/Categorias.java
@@ -0,0 +1,32 @@
+package Practica.EmpleosWalter.Modelo;
+
+public class Categorias {
+
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	@Override
+	public String toString() {
+		return "Categorias [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
+	}
+	
+	
+}

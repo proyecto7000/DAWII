652e3c1b8abba6297d312ca8b59a7a395439ef05
mikumikuha3215 <49241271+mikumikuha3215@users.noreply.github.com>
2020-09-21 21:46:11 +0200

    Practica11

--- Nicolas Zuniga de la Cruz/EmpleoNicolas/EmpleoNicolas/src/main/java/Nico/ils/EmpleoNicolas/controller/Categorias.java
+++ Nicolas Zuniga de la Cruz/EmpleoNicolas/EmpleoNicolas/src/main/java/Nico/ils/EmpleoNicolas/controller/Categorias.java
@@ -1,15 +1,25 @@
 package Nico.ils.EmpleoNicolas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import Nico.ils.EmpleoNicolas.model.Categoria;
+import Nico.ils.EmpleoNicolas.model.Vacante;
+import Nico.ils.EmpleoNicolas.service.ICategoriasService;
 
 @Controller
 
@@ -18,10 +28,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class Categorias {
 	
+	@Autowired
+	private ICategoriasService categoriaServicio; 
+	
 	@GetMapping("/index")
 	//@RequestMapping(value="/index", method=RequestMethod.GET)
 	public String mostrarIndex(Model model) {
 		
+		List<Categoria> listaCat = categoriaServicio.CargaCategorias();
+		model.addAttribute("CategoriasV", listaCat);
 		return "categorias/listCategorias";
 	}
 	
@@ -32,13 +47,39 @@ public class Categorias {
 			return "categorias/formCategoria";
 		}
 		
+//	@PostMapping("/save")
+//	//@RequestMapping(value="/save", method=RequestMethod.POST)
+//	public String nuevaCategoria(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion) {
+//		System.out.println("Nombre de categoria: " +nombre);
+//		System.out.println("Descripcion de Catetgoria: " +descripcion);
+//		return "categorias/listCategorias";
+//	}
+
 	@PostMapping("/save")
 	//@RequestMapping(value="/save", method=RequestMethod.POST)
-	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion) {
-		System.out.println("Nombre de categoria: " +nombre);
-		System.out.println("Descripcion de Catetgoria: " +descripcion);
-		return "categorias/listCategorias";
+	public String nuevaCategoria(Categoria miCategoria, BindingResult miResultado,RedirectAttributes atributo) {
+		
+		if(miResultado.hasErrors()) {
+			
+		for(ObjectError error: miResultado.getAllErrors()) {
+			System.out.println("Ocurrio un Error: "+error.getDefaultMessage());	
+		}
+		return "categorias/formCategoria";	
+		}
+		
+		String msj="Categoria Agregada Exitosamente";
+		atributo.addFlashAttribute("mensaje",msj);
+		
+		categoriaServicio.guardarVacante(miCategoria);
+		
+		System.out.println(miCategoria.toString());
+		
+		return "redirect:/categorias/index";
 	}
+	
+	
+	
+	
 	
 	@GetMapping("/delete")
 	public String borrar(@RequestParam("idVacante") int id, Model miModelo) {

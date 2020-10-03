dc38744ed6abd9c12b9476b5ebb38319ec431fec
Anthony <boriscueva78@gmail.com>
2020-10-03 10:18:31 -0500

    ANTHONY PRAC 16

--- Anthony-Pérez-Casali/JPA-Anthony/src/main/java/DAWII/ILS/JPAAnthony/JpaAnthonyApplication.java
+++ Anthony-Pérez-Casali/JPA-Anthony/src/main/java/DAWII/ILS/JPAAnthony/JpaAnthonyApplication.java
@@ -1,5 +1,9 @@
 package DAWII.ILS.JPAAnthony;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,8 +25,9 @@ public class JpaAnthonyApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		guardar();
+		guardartodas();
 	}
+	//METODO CREATE
 	private void guardar()
 	{
 		CATEGORIAS cat = new CATEGORIAS();
@@ -30,5 +35,106 @@ public class JpaAnthonyApplication implements CommandLineRunner {
 		cat.setDescripcion("Trabajos relacionados con Finanzas y Contabilidad");
 		repo.save(cat);
 		System.out.println(cat);
+	}
+	//METODO BUSCAR
+	private void buscarPorID()
+	{
+		Optional<CATEGORIAS> optional = repo.findById(1);
+		if (optional.isPresent()) 
+		{
+			System.out.println(optional.get());
+		}
+		else
+		{
+			System.out.println("CATEGORIA NO ENCONTRADA");
+		}
+	}
+	//METODOMODIFICAR
+	private void modificar()
+	{
+		Optional<CATEGORIAS> optional = repo.findById(1);
+		if (optional.isPresent()) 
+		{
+			CATEGORIAS catTmp = optional.get();
+			catTmp.setNombre("FINANZAS NUEVA");
+			catTmp.setDescripcion("DESCRIPCION NUEVA");
+			repo.save(catTmp);
+			System.out.println("MODIFICADA CON EXITO: " + optional.get());
+		}else
+		{
+			System.out.println("CATEGORIA NO ENCONTRADA");
+		}
+	}
+	//METODO ELIMINAR
+	private void eliminar()
+	{
+		int idcategoria = 2;
+		repo.deleteById(idcategoria);
+	}
+	//METODO DE CONTAR
+	private void contar()
+	{
+		long count = repo.count();
+		System.out.println("TOTAL DE CATEGORIAS: " + count);
+	}
+	//METODO ELIMINAR TODO
+	private void eliminartodo()
+	{
+		repo.deleteAll();
+	}
+	//METODO ENCONTRAR POR IDS
+	private void encontrarporIDs()
+	{
+		List<Integer> ids = new LinkedList<Integer>();
+		ids.add(1);
+		ids.add(4);
+		ids.add(6);
+		Iterable<CATEGORIAS> categorias = repo.findAllById(ids);
+		for (CATEGORIAS cat : categorias)
+			{
+				System.out.println(cat);
+			}
+	}
+	//METODO EXISTE id
+	private void existeID()
+	{
+		boolean existe = repo.existsById(50);
+		System.out.println("LA CATEGORIA EXISTE " + existe);
+	}	
+	//METODO BUSCAR TODO
+	private void buscartodos()
+	{
+		Iterable<CATEGORIAS> categorias =  repo.findAll();
+		for(CATEGORIAS cat : categorias)
+		{
+			System.out.println(cat);
+		}
+	}
+	
+	private List<CATEGORIAS> getListaCategorias()
+	{
+		List<CATEGORIAS> lista = new LinkedList<CATEGORIAS>();
+		
+		CATEGORIAS cat1 = new CATEGORIAS();
+		cat1.setNombre("programador de blockchain");
+		cat1.setDescripcion("Trabajos relacionados con bitcoin");
+		
+		CATEGORIAS cat2 = new CATEGORIAS();
+		cat2.setNombre("SOLDADURA/PINTURA");
+		cat2.setDescripcion("Trabajos relacionados con soldadura pintura y enderezado");
+		
+		CATEGORIAS cat3 = new CATEGORIAS();
+		cat3.setNombre("INGENIERO INDUSTRIAL");
+		cat3.setDescripcion("Trabajos relacionados con ingenieria industrial");
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		return lista;
+	}
+	private void guardartodas()
+	{
+		List<CATEGORIAS> categorias = getListaCategorias();
+		repo.saveAll(categorias);
 	}
 }

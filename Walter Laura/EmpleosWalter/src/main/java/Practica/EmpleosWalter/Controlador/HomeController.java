9b4246e52223a00da36966d10e9f8cf38219c665
walter2703 <59626631+walter2703@users.noreply.github.com>
2020-10-13 21:37:32 -0500

    practica 22

--- Walter Laura/EmpleosWalter/src/main/java/Practica/EmpleosWalter/Controlador/HomeController.java
+++ Walter Laura/EmpleosWalter/src/main/java/Practica/EmpleosWalter/Controlador/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import Practica.EmpleosWalter.Modelo.Vacantes;
 import Practica.EmpleosWalter.Servicio.VacantService;
@@ -23,6 +24,9 @@ public class HomeController {
 		model.addAttribute("Vacantes",listaS);
 		return "Listas";
 	}
-	
+	@ModelAttribute
+	public void  setGenericos(Model model) {
+		model.addAttribute("Vacantes",servicioVacantes.buscarDestacados());
+	}
 }
 
